9c436dc0ed41e479dab223360d616909324b9a3f
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2024-05-31 19:23:58 -0300

    layout and basic interface

---
+++ site-pessoal/src/components/Anchor.vue
@@ -0,0 +1,9 @@
+<script setup>
+    defineProps({
+        text: String
+    })
+</script>
+
+<template>
+    <a href="#" class="block p-1 hover:text-sky-300 transition duration-300 ease-in-out">{{ text }}</a>
+</template>
---
+++ site-pessoal/src/components/Content.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-col justify-center items-center font-semibold">
+        <h1 class="text-6xl text-sky-500">Hi, I am Jacob Reels</h1>
+        <h3 class="text-2xl text-left bg-clip-text text-transparent bg-gradient-to-r from-sky-600 to-sky-800">Front end developer</h3>
+        <h4 class="text-2xl text-left text-slate-200">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centurie</h4>
+
+        <button class="bg-sky-600 hover:bg-sky-800 transition duration-300 hover:text-sky-300 ease-in-out rounded-xl p-5 mt-4 cursor-pointer ">Contact me</button>
+    </div>
+</template>
